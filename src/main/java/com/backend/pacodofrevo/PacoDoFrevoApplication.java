195a749207ed5067c23541398a63188ad0dc2f78
Esmyrna <Esmyrnaoliveira13@gmail.com>
2023-11-26 19:18:31 -0300

    Criando configuração de ambiente e conexão com o banco

---
+++ src/main/java/com/backend/pacodofrevo/PacoDoFrevoApplication.java
@@ -0,0 +1,13 @@
+package com.backend.pacodofrevo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacoDoFrevoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacoDoFrevoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/pacodofrevo/PacoDoFrevoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.pacodofrevo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacoDoFrevoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
