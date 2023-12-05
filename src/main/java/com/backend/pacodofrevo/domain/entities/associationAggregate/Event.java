c326401b36912f0a741297f1a30506abaa9f6056
DALuckwu <dara.00000847256@unicap.br>
2023-12-04 22:42:59 -0300

    corrigindo erros da entidade

--- src/main/java/com/backend/pacodofrevo/domain/entities/associationAggregate/Event.java
+++ src/main/java/com/backend/pacodofrevo/domain/entities/associationAggregate/Event.java
@@ -1,18 +1,13 @@
 package com.backend.pacodofrevo.domain.entities.associationAggregate;
 
 import java.sql.Date;
-import java.time.Instant;
-import java.time.LocalDateTime;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
-import jakarta.persistence.TemporalType;
+
 
 public class Event {
     @Id
--- src/main/java/com/backend/pacodofrevo/domain/entities/associationAggregate/Member.java
+++ src/main/java/com/backend/pacodofrevo/domain/entities/associationAggregate/Member.java
@@ -1,13 +1,11 @@
 package com.backend.pacodofrevo.domain.entities.associationAggregate;
 
 import java.sql.Date;
-import java.time.LocalDateTime;
 
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
+
 
 public class Member {
     @Id
