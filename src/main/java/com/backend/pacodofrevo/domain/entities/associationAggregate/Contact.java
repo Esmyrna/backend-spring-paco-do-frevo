3d66b93a7baae1761e4d2f0d640494925803e8ba
Nande14 <nande.nave@gmail.com>
2023-12-04 22:17:02 -0300

    criando entidade

--- src/main/java/com/backend/pacodofrevo/domain/entities/associationAggregate/Contact.java
+++ src/main/java/com/backend/pacodofrevo/domain/entities/associationAggregate/Contact.java
@@ -1,5 +1,43 @@
 package com.backend.pacodofrevo.domain.entities.associationAggregate;
 
+import java.sql.Date;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
 public class Contact {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+
+    private String addressTo;
+
+    private String email;
+ 
+    private List<PhoneNumber> phoneNumbers;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedAt;
+
+    private String createdBy;
+
+    private String updatedBy;
+
+ 
+    private Association association;
+
+    private String associationId;
+
 }
