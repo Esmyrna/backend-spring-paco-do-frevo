e58bc8a54df2042b528610eaaa7eedc30456dcb9
DALuckwu <dara.00000847256@unicap.br>
2023-12-04 22:38:55 -0300

    criando entidade member

--- src/main/java/com/backend/pacodofrevo/domain/entities/associationAggregate/Member.java
+++ src/main/java/com/backend/pacodofrevo/domain/entities/associationAggregate/Member.java
@@ -1,5 +1,34 @@
 package com.backend.pacodofrevo.domain.entities.associationAggregate;
 
+import java.sql.Date;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Member {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+
+    private String name;
+
+    private String surname;
+
+    private String role;
+
+    private int actuationTimeInMonths;
+
+    private boolean isFrevoTheMainRevenueIncome;
+
+    private Association association;
+
+    private String associationId;
+
+    private Date createdAt;
+
+    private Date updatedAt;
+
+    private String createdBy;
+
+    private String updatedBy;
 }
